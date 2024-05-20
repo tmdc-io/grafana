2dcddd9053c692611e7b69a85f70d09c58f7e5f4
Suraj Singh <suraj@tmdc.io>
2024-05-20 14:21:37 +0530

    pep implementation done

--- pkg/login/heimdall/service.go
+++ pkg/login/heimdall/service.go
@@ -6,13 +6,12 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
-	"strings"
+	"time"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/services/org"
 )
@@ -33,13 +32,23 @@ type BasicUserInfo struct {
 }
 
 type AuthorizationRequest struct {
-	Token string `json:"token"`
+	Token   string                       `json:"token"`
+	Context *AuthorizationRequestContext `json:"context,omitempty"`
+}
+
+type AuthorizationRequestContext struct {
+	Predicate string        `json:"predicate"`
+	Object    ContextObject `json:"object"`
+}
+
+type ContextObject struct {
+	Tags []string `json:"Tags,omitempty"`
 }
 
 type AuthorizationResponse struct {
 	Allow  bool                `json:"allow,omitempty" binding:"required"`
 	Result AuthorizationResult `json:"result,omitempty"`
-	Error  string              `json:"error,omitempty"`
+	Error  interface{}         `json:"error,omitempty"`
 }
 
 type AuthorizationResult struct {
@@ -49,176 +58,106 @@ type AuthorizationResult struct {
 }
 
 const (
-	AuthorizePath      = "/api/v1/authorize"
-	ContentType        = "application/json"
-	GrafanaAdminTags   = "GRAFANA_ADMIN_ACCESS_TAGS"
-	GrafanaViewerTags  = "GRAFANA_VIEWER_ACCESS_TAGS"
-	HeimdallUseUnsafe  = "HEIMDALL_USE_UNSAFE"
-	HeimdallBaseUrlKey = "HEIMDALL_BASE_URL"
+	ContentType = "application/json"
 )
 
-// Authorize sends a POST request to the specified authorization URL
-// with the provided authorization request, using the given HTTP client.
-// It returns the authorization response and any encountered error.
-func Authorize(authorizeUrl string, authz AuthorizationRequest) (*AuthorizationResponse, error) {
+type Config struct {
+	HeimdallBaseUrlKey string `env:"HEIMDALL_URL" envDefault:"https://heimdall-api.heimdall.svc.cluster.local:32010/heimdall"`
+	AuthorizePath      string `env:"HEIMDALL_AUTH_URL" envDefault:"/api/v1/authorize"`
+	HeimdallUseUnsafe  string `env:"HEIMDALL_USE_UNSAFE" envDefault:"true"`
+}
+
+var Tags = []string{"dataos:type:grafana:admin", "dataos:type:grafana:view"}
+
+func checkAuthorization(accessToken string, tag string) (*AuthorizationResponse, error) {
 
 	myClient := client()
 
-	req, err := json.Marshal(authz)
+	cfg, err := LoadConfig()
 	if err != nil {
-		logger.Debug("Error marshaling JSON:", err)
-		return nil, err
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+
+	aR := AuthorizationRequest{
+		Token: accessToken,
+		Context: &AuthorizationRequestContext{
+			Predicate: "get",
+			Object: ContextObject{
+				Tags: []string{tag},
+			},
+		},
+	}
+
+	req, err := json.Marshal(aR)
+
+	if err != nil {
+		log.Printf("Error marshaling JSON:", err)
 	}
 	authReq := bytes.NewReader(req)
 
-	response, err := myClient.Post(authorizeUrl, ContentType, authReq)
+	response, err := myClient.Post(cfg.HeimdallBaseUrlKey+cfg.AuthorizePath, ContentType, authReq)
 	if err != nil {
-		logger.Debug("could not call the authorization resource: ", err)
-		return nil, err
+		fmt.Printf("Not able to reach heimdall")
 	}
-	if response == nil {
-		logger.Debug("user authorization failed: response is nil")
-		return nil, errors.New("user authorization failed: response is nil")
-	}
+
 	var ar AuthorizationResponse
 	responseContent, err := io.ReadAll(response.Body)
-	if err != nil {
-		logger.Debug("failed to ready response: ", err)
-		return nil, err
-	}
-	if len(responseContent) == 0 {
-		logger.Warn("response content is blank")
-		return nil, errors.New("response content is blank")
-	}
-	err = json.Unmarshal(responseContent, &ar)
-	if err != nil {
-		logger.Debug("could not unmarshal authorization response json: %s", err)
-		return nil, err
-	}
-	if response.StatusCode != 200 {
-		var errorResponse = ar.Error
-		if len(responseContent) == 0 {
-			errorResponse = response.Status
-		}
-		logger.Debug("user authorization failed: ", errorResponse)
-		return nil, errors.New(errorResponse)
-	}
+	json.Unmarshal(responseContent, &ar)
+
 	return &ar, nil
 }
 
-// AuthorizeUser performs user authorization using Heimdall.
-// It validates the user's token against Heimdall's authorization service
-// and updates the provided BasicUserInfo with appropriate role and admin status.
-// It returns an error if authorization fails or encounters any issues.
 func AuthorizeUser(token string, userInfo *BasicUserInfo) (*BasicUserInfo, error) {
-	logger.Info("calling heimdall for authorization...")
+	tags := Tags
 
-	heimdallBaseUrl := os.Getenv(HeimdallBaseUrlKey)
-	if len(heimdallBaseUrl) <= 0 {
-		logger.Warn(fmt.Sprintf("environment variable %s not configured", HeimdallBaseUrlKey))
-		return nil, errors.New(fmt.Sprintf("environment variable %s not configured", HeimdallBaseUrlKey))
-	}
-	adminTag := strings.ReplaceAll(os.Getenv(GrafanaAdminTags), " ", "")
-	if len(adminTag) <= 0 {
-		logger.Warn(fmt.Sprintf("environment variable %s not configured", GrafanaAdminTags))
-		return nil, errors.New(fmt.Sprintf("environment variable %s not configured", GrafanaAdminTags))
-	}
-	adminTags := strings.Split(adminTag, ",")
-	if len(adminTags) < 1 {
-		logger.Warn("no admin tags provided")
-		return nil, errors.New("no admin tags provided")
-	}
-	viewerTag := strings.ReplaceAll(os.Getenv(GrafanaViewerTags), " ", "")
-	if len(viewerTag) <= 0 {
-		logger.Warn(fmt.Sprintf("environment variable %s not configured", GrafanaViewerTags))
-		return nil, errors.New(fmt.Sprintf("environment variable %s not configured", GrafanaViewerTags))
-	}
-	viewerTags := strings.Split(viewerTag, ",")
-	if len(viewerTags) < 1 {
-		logger.Warn("no viewer tags provided")
-		return nil, errors.New("no viewer tags provided")
-	}
-	authorizeUrl := strings.TrimSuffix(heimdallBaseUrl, "/") + AuthorizePath
-	req := AuthorizationRequest{
-		Token: token,
-	}
-	ar, err := Authorize(authorizeUrl, req)
-	if err != nil {
-		logger.Debug("failure during the authorize request: %s", err)
-		return nil, err
-	}
-	if !ar.Allow {
-		return nil, errors.New("allow=false")
-	}
-	if ar.Result.ID == "" {
-		return nil, errors.New("failure during the authorize request, user id is nil")
-	}
-	if len(ar.Result.Tags) == 0 {
-		return nil, errors.New("no tags found for the user")
-	}
-
-	// Determine user role based on tags
-	if adminCommonTag(ar.Result.Tags, adminTags) {
-		admin = true
-		userInfo.Role = org.RoleAdmin
-		userInfo.IsGrafanaAdmin = &admin
-	} else if viewerCommonTag(ar.Result.Tags, viewerTags) {
-		userInfo.Role = org.RoleViewer
-		admin = false
-		userInfo.IsGrafanaAdmin = &admin
-	} else {
-		// userInfo.Role = org.RoleNone
-		// admin = false
-		// userInfo.IsGrafanaAdmin = &admin
+	for _, tag := range tags {
+		ar, err := checkAuthorization(token, tag)
+		if err != nil {
+			return nil, err
+		}
+		if ar.Allow {
+			if tag == Tags[0] {
+				admin = true
+				userInfo.Role = org.RoleAdmin
+				userInfo.IsGrafanaAdmin = &admin
+			} else {
+				userInfo.Role = org.RoleViewer
+				admin = false
+				userInfo.IsGrafanaAdmin = &admin
+			}
+		}
 		return nil, nil
 	}
-	return userInfo, nil
-}
 
-// adminCommonTag checks if there are any common tags between the user tags and admin tags.
-// It returns true if a admin tag is found; otherwise, it returns false.
-func adminCommonTag(userTags, adminTags []string) bool {
-	adminTagsMap := make(map[string]bool)
-	for _, adminTag := range adminTags {
-		adminTagsMap[adminTag] = true
-	}
-	for _, userTag := range userTags {
-		if adminTagsMap[userTag] {
-			return true
-		}
-	}
-	return false
-}
-
-// viewerCommonTag checks if there are any common tags between the user tags and viewer tags.
-// It returns true if a viewer tag is found; otherwise, it returns false.
-func viewerCommonTag(userTags, viewerTags []string) bool {
-	viewerTagsMap := make(map[string]bool)
-	for _, viewerTag := range viewerTags {
-		viewerTagsMap[viewerTag] = true
-	}
-	for _, userTag := range userTags {
-		if viewerTagsMap[userTag] {
-			return true
-		}
-	}
-	return false
+	return nil, nil
 }
 
 // client configures an HTTP client with TLS verification disabled.
 func client() *http.Client {
-	if os.Getenv(HeimdallUseUnsafe) == "true" {
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
+	if cfg.HeimdallUseUnsafe == "true" {
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
-		return &http.Client{Transport: tr}
-	} else if os.Getenv(HeimdallUseUnsafe) == "false" {
+		return &http.Client{Timeout: 10 * time.Second, Transport: tr}
+	} else if cfg.HeimdallUseUnsafe == "false" {
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: false},
 		}
-		return &http.Client{Transport: tr}
+		return &http.Client{Timeout: 10 * time.Second, Transport: tr}
 	} else {
 		fmt.Println("Value of HEIMDALL_USE_UNSAFE is neither true nor false")
 		return nil
 	}
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
 }
